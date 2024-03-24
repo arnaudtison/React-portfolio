68998fdd49dd89193f24121ea626755c3370ced9
Arnaud <arnaud.tison@student.hogent.be>
2024-03-24 14:32:47 +0100

    push2

--- src/Navigation/App.js
+++ src/Navigation/App.js
@@ -4,8 +4,6 @@ import Home from '../Home/Home';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Navigation from '../Navigation/Navigation';
-import Work from '../Work/Work';
-import Gallery from '../Gallery/Gallery'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
@@ -16,20 +14,14 @@ const App = () => {
       </div>
 
       <Switch>
-        <Route exact path="/">
+        <Route path="/">
           <Home />
         </Route>
-        <Route exact path="/about">
+        <Route path="/about">
           <About />
         </Route>
         <Route path="/contact">
           <Contact />
-        </Route>
-        <Route path="/work">
-          <Work />
-        </Route>
-        <Route path="/gallery">
-          <Gallery />
         </Route>
       </Switch>
     </Router>
